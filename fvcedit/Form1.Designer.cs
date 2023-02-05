19d5bec067d4539431c9c75c5f31fbd62c81a810
Matto <mattopootis@gmail.com>
2023-02-05 14:08:53 +0100

    Upload v002

--- fvcedit/Form1.Designer.cs
+++ fvcedit/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
@@ -38,9 +39,12 @@
 			this.gBar = new System.Windows.Forms.TrackBar();
 			this.rBar = new System.Windows.Forms.TrackBar();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
-			this.trackBar6 = new System.Windows.Forms.TrackBar();
-			this.trackBar4 = new System.Windows.Forms.TrackBar();
-			this.trackBar5 = new System.Windows.Forms.TrackBar();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.brightBar = new System.Windows.Forms.TrackBar();
+			this.satBar = new System.Windows.Forms.TrackBar();
+			this.hueBar = new System.Windows.Forms.TrackBar();
 			this.tabPage3 = new System.Windows.Forms.TabPage();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
@@ -55,9 +59,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.gBar)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.rBar)).BeginInit();
 			this.tabPage2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.brightBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.satBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.hueBar)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
@@ -68,9 +72,10 @@
 			this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
 			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Location = new System.Drawing.Point(15, 15);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(776, 350);
+			this.pictureBox1.Size = new System.Drawing.Size(970, 437);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
@@ -80,10 +85,11 @@
 			this.tabControl1.Controls.Add(this.tabPage1);
 			this.tabControl1.Controls.Add(this.tabPage2);
 			this.tabControl1.Controls.Add(this.tabPage3);
-			this.tabControl1.Location = new System.Drawing.Point(12, 368);
+			this.tabControl1.Location = new System.Drawing.Point(15, 460);
+			this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(564, 117);
+			this.tabControl1.Size = new System.Drawing.Size(705, 146);
 			this.tabControl1.TabIndex = 1;
 			// 
 			// tabPage1
@@ -94,10 +100,11 @@
 			this.tabPage1.Controls.Add(this.bBar);
 			this.tabPage1.Controls.Add(this.gBar);
 			this.tabPage1.Controls.Add(this.rBar);
-			this.tabPage1.Location = new System.Drawing.Point(4, 24);
+			this.tabPage1.Location = new System.Drawing.Point(4, 29);
+			this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
 			this.tabPage1.Name = "tabPage1";
-			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(556, 89);
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+			this.tabPage1.Size = new System.Drawing.Size(697, 113);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "RGBmanager";
 			this.tabPage1.UseVisualStyleBackColor = true;
@@ -105,117 +112,162 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(226, 54);
+			this.label4.Location = new System.Drawing.Point(282, 68);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(13, 15);
+			this.label4.Size = new System.Drawing.Size(17, 20);
 			this.label4.TabIndex = 5;
 			this.label4.Text = "0";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(116, 54);
+			this.label3.Location = new System.Drawing.Point(145, 68);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(13, 15);
+			this.label3.Size = new System.Drawing.Size(17, 20);
 			this.label3.TabIndex = 4;
 			this.label3.Text = "0";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(6, 54);
+			this.label2.Location = new System.Drawing.Point(8, 68);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(13, 15);
+			this.label2.Size = new System.Drawing.Size(17, 20);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "0";
 			// 
 			// bBar
 			// 
 			this.bBar.BackColor = System.Drawing.Color.SteelBlue;
-			this.bBar.Location = new System.Drawing.Point(226, 6);
+			this.bBar.Location = new System.Drawing.Point(282, 8);
+			this.bBar.Margin = new System.Windows.Forms.Padding(4);
 			this.bBar.Maximum = 255;
 			this.bBar.Minimum = -255;
 			this.bBar.Name = "bBar";
-			this.bBar.Size = new System.Drawing.Size(104, 45);
+			this.bBar.Size = new System.Drawing.Size(130, 56);
 			this.bBar.TabIndex = 2;
 			this.bBar.Scroll += new System.EventHandler(this.trackBar3_Scroll);
 			// 
 			// gBar
 			// 
 			this.gBar.BackColor = System.Drawing.Color.ForestGreen;
-			this.gBar.Location = new System.Drawing.Point(116, 6);
+			this.gBar.Location = new System.Drawing.Point(145, 8);
+			this.gBar.Margin = new System.Windows.Forms.Padding(4);
 			this.gBar.Maximum = 255;
 			this.gBar.Minimum = -255;
 			this.gBar.Name = "gBar";
-			this.gBar.Size = new System.Drawing.Size(104, 45);
+			this.gBar.Size = new System.Drawing.Size(130, 56);
 			this.gBar.TabIndex = 1;
 			this.gBar.Scroll += new System.EventHandler(this.trackBar2_Scroll);
 			// 
 			// rBar
 			// 
 			this.rBar.BackColor = System.Drawing.Color.Maroon;
-			this.rBar.Location = new System.Drawing.Point(6, 6);
+			this.rBar.Location = new System.Drawing.Point(8, 8);
+			this.rBar.Margin = new System.Windows.Forms.Padding(4);
 			this.rBar.Maximum = 255;
 			this.rBar.Minimum = -255;
 			this.rBar.Name = "rBar";
-			this.rBar.Size = new System.Drawing.Size(104, 45);
+			this.rBar.Size = new System.Drawing.Size(130, 56);
 			this.rBar.TabIndex = 0;
 			this.rBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
 			// 
 			// tabPage2
 			// 
-			this.tabPage2.Controls.Add(this.trackBar6);
-			this.tabPage2.Controls.Add(this.trackBar4);
-			this.tabPage2.Controls.Add(this.trackBar5);
-			this.tabPage2.Location = new System.Drawing.Point(4, 24);
+			this.tabPage2.Controls.Add(this.label7);
+			this.tabPage2.Controls.Add(this.label6);
+			this.tabPage2.Controls.Add(this.label5);
+			this.tabPage2.Controls.Add(this.brightBar);
+			this.tabPage2.Controls.Add(this.satBar);
+			this.tabPage2.Controls.Add(this.hueBar);
+			this.tabPage2.Location = new System.Drawing.Point(4, 29);
+			this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
 			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(556, 89);
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+			this.tabPage2.Size = new System.Drawing.Size(697, 113);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "HSBmanager";
 			this.tabPage2.UseVisualStyleBackColor = true;
 			// 
-			// trackBar6
+			// label7
 			// 
-			this.trackBar6.BackColor = System.Drawing.Color.OldLace;
-			this.trackBar6.Location = new System.Drawing.Point(226, 6);
-			this.trackBar6.Maximum = 100;
-			this.trackBar6.Name = "trackBar6";
-			this.trackBar6.Size = new System.Drawing.Size(104, 45);
-			this.trackBar6.TabIndex = 5;
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(282, 68);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(17, 20);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "0";
 			// 
-			// trackBar4
+			// label6
 			// 
-			this.trackBar4.BackColor = System.Drawing.Color.Goldenrod;
-			this.trackBar4.Location = new System.Drawing.Point(116, 6);
-			this.trackBar4.Maximum = 100;
-			this.trackBar4.Name = "trackBar4";
-			this.trackBar4.Size = new System.Drawing.Size(104, 45);
-			this.trackBar4.TabIndex = 4;
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(145, 68);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(17, 20);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "0";
 			// 
-			// trackBar5
+			// label5
 			// 
-			this.trackBar5.BackColor = System.Drawing.Color.Maroon;
-			this.trackBar5.Location = new System.Drawing.Point(6, 6);
-			this.trackBar5.Maximum = 360;
-			this.trackBar5.Name = "trackBar5";
-			this.trackBar5.Size = new System.Drawing.Size(104, 45);
-			this.trackBar5.TabIndex = 3;
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(8, 68);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(17, 20);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "0";
+			// 
+			// brightBar
+			// 
+			this.brightBar.BackColor = System.Drawing.Color.OldLace;
+			this.brightBar.Location = new System.Drawing.Point(282, 8);
+			this.brightBar.Margin = new System.Windows.Forms.Padding(4);
+			this.brightBar.Maximum = 100;
+			this.brightBar.Name = "brightBar";
+			this.brightBar.Size = new System.Drawing.Size(130, 56);
+			this.brightBar.TabIndex = 5;
+			this.brightBar.Scroll += new System.EventHandler(this.brightBar_Scroll);
+			// 
+			// satBar
+			// 
+			this.satBar.BackColor = System.Drawing.Color.Goldenrod;
+			this.satBar.Location = new System.Drawing.Point(145, 8);
+			this.satBar.Margin = new System.Windows.Forms.Padding(4);
+			this.satBar.Maximum = 100;
+			this.satBar.Name = "satBar";
+			this.satBar.Size = new System.Drawing.Size(130, 56);
+			this.satBar.TabIndex = 4;
+			this.satBar.Scroll += new System.EventHandler(this.satBar_Scroll);
+			// 
+			// hueBar
+			// 
+			this.hueBar.BackColor = System.Drawing.Color.Maroon;
+			this.hueBar.Location = new System.Drawing.Point(8, 8);
+			this.hueBar.Margin = new System.Windows.Forms.Padding(4);
+			this.hueBar.Maximum = 360;
+			this.hueBar.Name = "hueBar";
+			this.hueBar.Size = new System.Drawing.Size(130, 56);
+			this.hueBar.TabIndex = 3;
+			this.hueBar.Scroll += new System.EventHandler(this.hueBar_Scroll);
 			// 
 			// tabPage3
 			// 
-			this.tabPage3.Location = new System.Drawing.Point(4, 24);
+			this.tabPage3.Location = new System.Drawing.Point(4, 29);
+			this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
 			this.tabPage3.Name = "tabPage3";
-			this.tabPage3.Size = new System.Drawing.Size(556, 89);
+			this.tabPage3.Size = new System.Drawing.Size(697, 113);
 			this.tabPage3.TabIndex = 2;
 			this.tabPage3.Text = "Effectmanager";
 			this.tabPage3.UseVisualStyleBackColor = true;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(683, 433);
+			this.button1.Location = new System.Drawing.Point(854, 541);
+			this.button1.Margin = new System.Windows.Forms.Padding(4);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(105, 23);
+			this.button1.Size = new System.Drawing.Size(131, 29);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "Load frame";
 			this.button1.UseVisualStyleBackColor = true;
@@ -223,9 +275,10 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(578, 433);
+			this.button2.Location = new System.Drawing.Point(722, 541);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(99, 23);
+			this.button2.Size = new System.Drawing.Size(124, 29);
 			this.button2.TabIndex = 3;
 			this.button2.Text = "Save frame";
 			this.button2.UseVisualStyleBackColor = true;
@@ -233,25 +286,28 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(578, 404);
+			this.textBox1.Location = new System.Drawing.Point(722, 505);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(210, 23);
+			this.textBox1.Size = new System.Drawing.Size(262, 27);
 			this.textBox1.TabIndex = 4;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(578, 386);
+			this.label1.Location = new System.Drawing.Point(722, 482);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(92, 15);
+			this.label1.Size = new System.Drawing.Size(115, 20);
 			this.label1.TabIndex = 5;
 			this.label1.Text = "Frame filename:";
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(683, 462);
+			this.button3.Location = new System.Drawing.Point(854, 578);
+			this.button3.Margin = new System.Windows.Forms.Padding(4);
 			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(105, 23);
+			this.button3.Size = new System.Drawing.Size(131, 29);
 			this.button3.TabIndex = 6;
 			this.button3.Text = "Apply changes";
 			this.button3.UseVisualStyleBackColor = true;
@@ -259,9 +315,10 @@
 			// 
 			// button4
 			// 
-			this.button4.Location = new System.Drawing.Point(578, 462);
+			this.button4.Location = new System.Drawing.Point(722, 578);
+			this.button4.Margin = new System.Windows.Forms.Padding(4);
 			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(99, 23);
+			this.button4.Size = new System.Drawing.Size(124, 29);
 			this.button4.TabIndex = 7;
 			this.button4.Text = "Reset changes";
 			this.button4.UseVisualStyleBackColor = true;
@@ -269,9 +326,9 @@
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-			this.ClientSize = new System.Drawing.Size(800, 497);
+			this.ClientSize = new System.Drawing.Size(1000, 621);
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.label1);
@@ -281,9 +338,11 @@
 			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.pictureBox1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.MaximizeBox = false;
 			this.Name = "Form1";
-			this.Text = "FormVideoColoredit";
+			this.Text = "FormVideoColoredit v002";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.tabControl1.ResumeLayout(false);
 			this.tabPage1.ResumeLayout(false);
@@ -293,9 +352,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.rBar)).EndInit();
 			this.tabPage2.ResumeLayout(false);
 			this.tabPage2.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.brightBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.satBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.hueBar)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -315,13 +374,16 @@
 		private TrackBar bBar;
 		private TrackBar gBar;
 		private TrackBar rBar;
-		private TrackBar trackBar5;
+		private TrackBar hueBar;
 		private Button button3;
-		private TrackBar trackBar6;
-		private TrackBar trackBar4;
+		private TrackBar brightBar;
+		private TrackBar satBar;
 		private Label label4;
 		private Label label3;
 		private Label label2;
 		private Button button4;
+		private Label label7;
+		private Label label6;
+		private Label label5;
 	}
 }
--- fvcedit/Form1.cs
+++ fvcedit/Form1.cs
@@ -17,6 +17,94 @@ namespace fvcedit
 			);
 		}
 
+
+		// https://stackoverflow.com/a/3837824
+		public static Color FromAhsb(int alpha, float hue, float saturation, float brightness)
+		{
+
+			if (0 > alpha || 255 < alpha)
+			{
+				throw new ArgumentOutOfRangeException("alpha", alpha,
+				  "Value must be within a range of 0 - 255.");
+			}
+			if (0f > hue || 360f < hue)
+			{
+				throw new ArgumentOutOfRangeException("hue", hue,
+				  "Value must be within a range of 0 - 360.");
+			}
+			if (0f > saturation || 1f < saturation)
+			{
+				throw new ArgumentOutOfRangeException("saturation", saturation,
+				  "Value must be within a range of 0 - 1.");
+			}
+			if (0f > brightness || 1f < brightness)
+			{
+				throw new ArgumentOutOfRangeException("brightness", brightness,
+				  "Value must be within a range of 0 - 1.");
+			}
+
+			if (0 == saturation)
+			{
+				return Color.FromArgb(alpha, Convert.ToInt32(brightness * 255),
+				  Convert.ToInt32(brightness * 255), Convert.ToInt32(brightness * 255));
+			}
+
+			float fMax, fMid, fMin;
+			int iSextant, iMax, iMid, iMin;
+
+			if (0.5 < brightness)
+			{
+				fMax = brightness - (brightness * saturation) + saturation;
+				fMin = brightness + (brightness * saturation) - saturation;
+			}
+			else
+			{
+				fMax = brightness + (brightness * saturation);
+				fMin = brightness - (brightness * saturation);
+			}
+
+			iSextant = (int)Floor(hue / 60f);
+			if (300f <= hue)
+			{
+				hue -= 360f;
+			}
+			hue /= 60f;
+			hue -= 2f * (float)Floor(((iSextant + 1f) % 6f) / 2f);
+			if (0 == iSextant % 2)
+			{
+				fMid = hue * (fMax - fMin) + fMin;
+			}
+			else
+			{
+				fMid = fMin - hue * (fMax - fMin);
+			}
+
+			iMax = Convert.ToInt32(fMax * 255);
+			iMid = Convert.ToInt32(fMid * 255);
+			iMin = Convert.ToInt32(fMin * 255);
+
+			switch (iSextant)
+			{
+				case 1:
+					return Color.FromArgb(alpha, iMid, iMax, iMin);
+				case 2:
+					return Color.FromArgb(alpha, iMin, iMax, iMid);
+				case 3:
+					return Color.FromArgb(alpha, iMin, iMid, iMax);
+				case 4:
+					return Color.FromArgb(alpha, iMid, iMin, iMax);
+				case 5:
+					return Color.FromArgb(alpha, iMax, iMin, iMid);
+				default:
+					return Color.FromArgb(alpha, iMax, iMid, iMin);
+			}
+		}
+
+		static Color incrementHsb(Color i, float hue, float sat, float brightness)
+		{
+			return FromAhsb(i.A, (i.GetHue() + hue) % 360f, Min(i.GetSaturation() + sat, 1f), Min(i.GetBrightness() + brightness, 1f));
+		}
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -24,20 +112,17 @@ namespace fvcedit
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-
+			finalImage.Save(textBox1.Text);
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			workingImage = (Bitmap)Image.FromFile(textBox1.Text);
-			finalImage = (Bitmap)workingImage.Clone();
-			pictureBox1.BackgroundImage = (Bitmap)finalImage.Clone();
+			button4_Click(sender, e);
 		}
 
-		private void button3_Click(object sender, EventArgs e)
+		void rgbManager()
 		{
-			finalImage = (Bitmap)workingImage.Clone();
-
 			for (int y = 0; y < finalImage.Height; y++)
 			{
 				for (int x = 0; x < finalImage.Width; x++)
@@ -46,17 +131,35 @@ namespace fvcedit
 						x, y,
 						incrementRgb(
 							finalImage.GetPixel(x, y),
-							rBar.Value, gBar.Value, bBar.Value
-						)
-					);
+							rBar.Value, gBar.Value, bBar.Value));
 				}
 			}
 
-			Debug.WriteLine("processing done");
+			Debug.WriteLine("rgbmanager done");
+		}
+		void hsbManager()
+		{
+			for (int y = 0; y < finalImage.Height; y++)
+			{
+				for (int x = 0; x < finalImage.Width; x++)
+				{
+					finalImage.SetPixel(
+						x, y,
+						incrementHsb(
+							finalImage.GetPixel(x, y),
+							hueBar.Value, satBar.Value/100f, brightBar.Value/100f));
+				}
+			}
 
+			Debug.WriteLine("hsbmanager done");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			finalImage = (Bitmap)workingImage.Clone();
+			if (!(rBar.Value == 0 && gBar.Value == 0 && bBar.Value == 0)) rgbManager();
+			if (!(hueBar.Value == 0 && satBar.Value == 0 && brightBar.Value == 0)) hsbManager();
 			pictureBox1.BackgroundImage = (Bitmap)finalImage.Clone();
-
-			Debug.WriteLine("displaying done");
 		}
 
 		private void trackBar1_Scroll(object sender, EventArgs e)
@@ -82,6 +185,25 @@ namespace fvcedit
 			rBar.Value = 0; trackBar1_Scroll(sender, e);
 			gBar.Value = 0; trackBar2_Scroll(sender, e);
 			bBar.Value = 0; trackBar3_Scroll(sender, e);
+
+			hueBar.Value = 0; hueBar_Scroll(sender, e);
+			satBar.Value = 0; satBar_Scroll(sender, e);
+			brightBar.Value = 0; brightBar_Scroll(sender, e);
+		}
+
+		private void hueBar_Scroll(object sender, EventArgs e)
+		{
+			label5.Text = hueBar.Value.ToString() + "°";
+		}
+
+		private void satBar_Scroll(object sender, EventArgs e)
+		{
+			label6.Text = satBar.Value.ToString() + "%";
+		}
+
+		private void brightBar_Scroll(object sender, EventArgs e)
+		{
+			label7.Text = brightBar.Value.ToString() + "%";
 		}
 	}
 }
